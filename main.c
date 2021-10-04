9377e9809b7b05e1f8340f563c22141899091863
shane-decatur <sdecatur20@stuy.edu>
2021-10-04 11:36:49 -0400

    wrote second function

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//1
 void arrayToString(int arr[], int size){
   int i;
   printf("[ ");
@@ -8,9 +9,23 @@ void arrayToString(int arr[], int size){
   }
   printf("]\n");
 }
+//2
+int arrayAverage(int arr[], int size){
+  int i;
+  int sum = 0;
+  for (i = 0; i < size; i++){
+    sum += arr[i];
+  }
+  return sum / size;
+}
+//3
+
 
 int main(){
   int arr[5] = {12,5,2,1,9};
+  //1
   arrayToString(arr,5);
+  //2
+  printf("Average of Values: %d\n",arrayAverage(arr,5));
   return 0;
 }
