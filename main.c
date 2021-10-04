04c1d95df6268dddd40766feeab0e0d02b96c52d
shane-decatur <sdecatur20@stuy.edu>
2021-10-04 11:44:01 -0400

    wrote third function

--- main.c
+++ main.c
@@ -19,7 +19,12 @@ int arrayAverage(int arr[], int size){
   return sum / size;
 }
 //3
-
+void copyArrays(int *arr1, int *arr2, int size){
+  int i;
+  for (i = 0; i < size; i++){
+    arr2[i] = arr1[i];
+  }
+}
 
 int main(){
   int arr[5] = {12,5,2,1,9};
@@ -27,5 +32,10 @@ int main(){
   arrayToString(arr,5);
   //2
   printf("Average of Values: %d\n",arrayAverage(arr,5));
+  //3
+  int copy[5];
+  copyArrays(arr,copy,5);
+  arrayToString(arr,5);
+  arrayToString(copy,5);
   return 0;
 }
